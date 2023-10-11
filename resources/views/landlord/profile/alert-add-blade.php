2f7c9fdbcf1ab2b9590d1c8ab01a3ac4548dee76
Ayesha <hbdeveloper77@gmail.com>
2023-10-11 15:21:05 +0500

    updated

---
+++ resources/views/landlord/profile/alert-add-blade.php
@@ -0,0 +1,25 @@
+<!--Closable Alerts start -->
+<section id="alerts-closable">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header">
+          <h4 class="card-title">Closable Alerts</h4>
+        </div>
+        <div class="card-body">
+          <p class="card-text">
+          </p>
+          <div class="demo-spacing-0">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+              <div class="alert-body">
+              Success! Profile has been updated successfully.
+              </div>
+              <button type="button"  class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--Closable Alerts end -->
--- resources/views/landlord/property/index.blade.php
+++ resources/views/landlord/property/index.blade.php
@@ -43,12 +43,7 @@
               <td>HOUSE</td>
               <td>gzwr</td>
               <td>90</td>
-              <div>
-              <td>
-                <button>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye font-medium-2 text-body"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
-                </button></td>
-              </div>
+              <td><i data-feather='eye'></i></td>
             </tbody>
           </table>
         </div>
