5dab409a9152be7e54b851ced2a38a1a09dd65b9
hybreathe43 <hbdeveloper.43@gmail.com>
2023-10-12 17:45:34 +0500

    update

--- resources/views/landlord/leases/index.blade.php
+++ resources/views/landlord/leases/index.blade.php
@@ -35,7 +35,7 @@
                 <th>Statement</th>
               </tr>
             </thead>
-            <tbody>
+            {{-- <tbody>
               
                 <td>456</td>
                 <td>098</td>
@@ -48,7 +48,7 @@
                 <td>
                 <span class="badge rounded-pill badge-light-success me-1">Active</span></td>
               
-            </tbody>
+            </tbody> --}}
           </table>
         </div>
       </div>
@@ -75,5 +75,131 @@
 @endsection
 @section('page-script')
   {{-- Page js files --}}
-  <script src="{{ asset(mix('js/scripts/tables/table-datatables-basic.js')) }}"></script>
-@endsection
+  {{-- <script src="{{ asset(mix('js/scripts/tables/table-datatables-basic.js')) }}"></script> --}}
+  <script>
+     
+    $('.dt-multilingual').DataTable({
+        ajax:'{{ asset('data/table-datatable.json') }}',
+        columns: [
+          { data: 'responsive_id' },
+          { data: 'salary' },
+          { data: 'full_name' },
+          { data: 'city' },
+          { data: 'age' },
+          { data: 'start_date' },
+          { data: 'start_date' },
+          { data: 'status' },
+          { data: '' }
+        ],
+        columnDefs: [
+          // {
+          //   // For Responsive
+          //   className: 'control',
+          //   orderable: false,
+          //   targets: 0
+          // },
+          {
+            // Label
+            targets: -2,
+            render: function (data, type, full, meta) {
+              var $status_number = full['status'];
+              var $status = {
+                1: { title: 'Active', class: 'badge-light-success' },
+                2: { title: 'Unactive', class: ' badge-light-danger' },
+                3: { title: 'Active', class: ' badge-light-success' },
+                4: { title: 'Unactive', class: ' badge-light-danger' },
+                5: { title: 'Active', class: ' badge-light-success' }
+              };
+              if (typeof $status[$status_number] === 'undefined') {
+                return data;
+              }
+              return (
+                '<span class="badge rounded-pill ' +
+                $status[$status_number].class +
+                '">' +
+                $status[$status_number].title +
+                '</span>'
+              );
+            }
+          },
+          {
+            // Actions
+            targets: -1,
+            title: 'Actions',
+            orderable: false,
+            render: function (data, type, full, meta) {
+              return (
+                // '<div class="d-inline-flex">' +
+                // '<a class="pe-1 dropdown-toggle hide-arrow text-primary" data-bs-toggle="dropdown">' +
+                // feather.icons['more-vertical'].toSvg({ class: 'font-small-4' }) +
+                // '</a>' +
+                // '<div class="dropdown-menu dropdown-menu-end">' +
+                // // '<a href="javascript:;" class="dropdown-item">' +
+                // // feather.icons['file-text'].toSvg({ class: 'me-50 font-small-4' }) +
+                // // 'Details</a>' +
+                // '<a href="javascript:;" class="dropdown-item">' +
+                // feather.icons['edit'].toSvg({ class: 'me-50 font-medium-4 text-success' }) +
+                // 'Edit</a>' +
+                // '<a href="javascript:;" class="dropdown-item delete-record">' +
+                // feather.icons['trash-2'].toSvg({ class: 'me-50 font-medium-4 text-danger' }) +
+                // 'Delete</a>' +
+                // '</div>' +
+                // '</div>' 
+                // '<a href="javascript:;" class="item-edit pe-2">' +
+                // feather.icons['eye'].toSvg({ class: 'font-medium-4' }) +
+                // '</a>' +
+                '<a href="javascript:;" class="item-edit">' +
+                feather.icons['file-text'].toSvg({ class: 'font-medium-4' }) +
+                '</a>'
+              );
+            }
+          }
+        ],
+      //   language: {
+      //     url: '//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/' + lang + '.json',
+      //     paginate: {
+      //       // remove previous & next text from pagination
+      //       previous: '&nbsp;',
+      //       next: '&nbsp;'
+      //     }
+      //   },
+        dom: '<"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
+        displayLength: 7,
+        lengthMenu: [7, 10, 25, 50, 75, 100],
+        responsive: {
+          details: {
+            display: $.fn.dataTable.Responsive.display.modal({
+              header: function (row) {
+                var data = row.data();
+                return 'Details of ' + data['full_name'];
+              }
+            }),
+            type: 'column',
+            renderer: function (api, rowIdx, columns) {
+              var data = $.map(columns, function (col, i) {
+                return col.title !== '' // ? Do not show row in modal popup if title is blank (for check box)
+                  ? '<tr data-dt-row="' +
+                      col.rowIdx +
+                      '" data-dt-column="' +
+                      col.columnIndex +
+                      '">' +
+                      '<td>' +
+                      col.title +
+                      ':' +
+                      '</td> ' +
+                      '<td>' +
+                      col.data +
+                      '</td>' +
+                      '</tr>'
+                  : '';
+              }).join('');
+  
+              return data ? $('<table class="table"/>').append('<tbody>' + data + '</tbody>') : false;
+            }
+          }
+        }
+      });
+   
+  </script>
+
+  @endsection
