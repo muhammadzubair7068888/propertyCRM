ddf0b98fec1a731876be450901d771085d7ea0b3
ibrarahmed12 <hbdeveloper.39@gmail.com>
2023-11-24 16:39:21 +0500

    update

---
+++ resources/views/admin/repair/action.blade.php
@@ -0,0 +1,22 @@
+<div class="modal fade" id="actionmodel" tabindex="-1" aria-labelledby="addNewCardTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-right" style="max-width: 10%;">
+      <div class="modal-content">
+        <div class="modal-header bg-transparent">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body ">
+
+                <ul>
+                    <li>Delete</li>
+                    <li>Update</li>
+                    <li>Block</li>
+                </ul>
+
+
+
+          <!-- form -->
+
+        </div>
+      </div>
+    </div>
+  </div>
---
+++ resources/views/landlord/utilities/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts/contentLayoutMaster')
+
+@section('title', 'Landlord')
+
+@section('content')
+ this is utility page
+@endsection
+
